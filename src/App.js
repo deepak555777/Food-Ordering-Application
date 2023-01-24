375525e79d65d85bc7271bcfc0a1b742f18929c1
Deepak <deepak9478264417@gmail.com>
2023-01-24 14:37:15 -0800

    Added Meals,Card and some other things

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, { Fragment } from 'react'
+import Header from './Components/Header/Header'
+import Meals from './Components/Meals/Meals'
+import Cart from './Components/Cart/Cart'
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    <Fragment>
+<Header/>
+<Meals />
+<Cart/>
+
+
+    </Fragment>
+  )
 }
 
-export default App;
+export default App
